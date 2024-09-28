6aad1b5d1f729dfca3465baa9653422cfe5463e9
Patryk Orłowski <orlowski.dev@gmail.com>
2024-09-28 10:39:09 +0200

    Updated app-view and windows

--- components/views/app-view/component.tsx
+++ components/views/app-view/component.tsx
@@ -8,6 +8,7 @@ import "./styles.css";
 
 const AppView = () => {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+  const [previewOnly, setPreviewOnly] = useState(false);
   const editorContentRef = useRef<HTMLTextAreaElement>(null);
   const mainWrapperRef = useRef<HTMLDivElement>(null);
 
@@ -15,7 +16,19 @@ const AppView = () => {
     setIsSidebarVisible((prev) => !prev);
   }, []);
 
-  const onEditorBntClick = () => {};
+  const addClassPreviewOnly = () => {
+    if (!mainWrapperRef.current) return;
+
+    mainWrapperRef.current.classList.add("only-preview");
+    setPreviewOnly(true);
+  };
+
+  const removeClassPreviewOnly = () => {
+    if (!mainWrapperRef.current) return;
+
+    mainWrapperRef.current.classList.remove("only-preview");
+    setPreviewOnly(false);
+  };
 
   return (
     <div className={`app-view ${isSidebarVisible ? "sv" : "si"} `}>
@@ -28,10 +41,16 @@ const AppView = () => {
         <div className="main-wrapper" ref={mainWrapperRef}>
           <EditorWindow
             ref={editorContentRef}
-            onHeaderBtnClick={onEditorBntClick}
+            onHeaderBtnClick={addClassPreviewOnly}
           />
           <div className="separator"></div>
-          <PreviewWindow />
+          <PreviewWindow
+            onHeaderBtnClick={() => {
+              if (previewOnly) removeClassPreviewOnly();
+              else addClassPreviewOnly();
+            }}
+            buttonEvent={previewOnly ? "close-preview" : "only-preview"}
+          />
         </div>
       </div>
     </div>
--- components/windows/preview-window/component.tsx
+++ components/windows/preview-window/component.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import { EyeIcon } from "@/components/icons";
+import { EyeCloseIcon, EyeIcon } from "@/components/icons";
 import { Window } from "@/components/windows";
 import "./styles.css";
 
 export interface PreviewWindowProps {
   onHeaderBtnClick: () => void;
+  buttonEvent: "only-preview" | "close-preview";
 }
 
-const PreviewWindow = ({ onHeaderBtnClick }: PreviewWindowProps) => {
+const PreviewWindow = ({
+  onHeaderBtnClick,
+  buttonEvent,
+}: PreviewWindowProps) => {
   return (
     <Window
       windowTitle="Preview"
       windowClassName="preview-window"
-      headerButton={<button onClick={onHeaderBtnClick}>{<EyeIcon />}</button>}
+      headerButton={
+        <button onClick={onHeaderBtnClick}>
+          {buttonEvent === "only-preview" ? <EyeIcon /> : <EyeCloseIcon />}
+        </button>
+      }
     >
       <div className="preview-wrapper">
         <div className="preview">this is preview</div>
